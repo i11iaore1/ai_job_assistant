364434cda25a7026cf44ee211c8230abf9ce1580
partrick <orel.228.337@gmail.com>
2026-04-12 10:10:31 +0300

    Added more convenient repr for models

--- backend/sa_service/models/base_model.py
+++ backend/sa_service/models/base_model.py
@@ -11,3 +11,10 @@ class BaseModel(DeclarativeBase):
     updated_at: Mapped[datetime] = mapped_column(
         server_default=text("TIMEZONE('utc', now())")
     )
+
+    def __repr__(self) -> str:
+        columns = [
+            f"{column_name}={getattr(self, column_name)}"
+            for column_name in self.__table__.columns.keys()
+        ]
+        return f"<{self.__class__.__name__} {', '.join(columns)}>"
